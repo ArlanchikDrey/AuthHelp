2b6f8ba6ded2a0ea56478fae25666ed952fcd9d3
ArlanchikDrey <arlanchikdrey@yandex.ru>
2018-11-22 20:53:15 +0300

    new

--- app/src/main/java/com/example/authhelp/MainActivity.java
+++ app/src/main/java/com/example/authhelp/MainActivity.java
@@ -11,7 +11,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    ////
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
