c55a383ac6c072c10876a4a06b6a9ca58f91afb7
ArlanchikDrey <arlanchikdrey@yandex.ru>
2018-11-23 13:46:00 +0300

    Add code on LoginFragment and delete StartActivity

--- app/src/main/java/com/example/authhelp/fragment/LoginFragment.java
+++ app/src/main/java/com/example/authhelp/fragment/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.example.authhelp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,10 +9,110 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.authhelp.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
 public class LoginFragment extends Fragment {
+
+    SignInButton btn_sing;
+    FirebaseAuth mAuth; //Данные об авторизации
+    private FirebaseUser user; //Данные о пользователи
+    private DatabaseReference databaseReference ; //Ссылка на БД
+    GoogleSignInClient mGoogleClient;
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragmen_login,container,false);
+        btn_sing=view.findViewById(R.id.btn_sign);
+
+        mAuth=FirebaseAuth.getInstance();
+        btn_sing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleClient.getSignInIntent();
+                startActivityForResult(signInIntent, 101);
+            }
+        });
+
+        //Настройки Входа В Аккаунт Google
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleClient= GoogleSignIn.getClient(getContext(),gso);
         return super.onCreateView(inflater, container, savedInstanceState);
     }
+
+    //все связанное с гугл
+   /* @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Результат, возвращенный при запуске Intent из GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == 101) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Вход в Google был успешным, аутентификация с Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Ошибка входа в Google
+
+            }}}//конец метода activresult
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) { // Успешный вход, обновление пользовательского
+                            //проверка на наличие данных о пользователе в базе
+                            user=mAuth.getCurrentUser();
+                            final  String user_id= mAuth.getCurrentUser().getUid();
+                            databaseReference= FirebaseDatabase.getInstance().getReference("Users");
+                            databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    if(dataSnapshot.hasChild(user_id)){
+
+                                    }else{
+                                        String name_google=user.getDisplayName();
+                                        Uri ava=user.getPhotoUrl();
+                                        Map<String,String> usermap =new HashMap<>();
+                                        usermap.put("Avatar", ava.toString());
+                                        usermap.put("name", name_google);
+                                        usermap.put("family", "");
+                                        databaseReference.child(user_id).setValue(usermap);
+
+                                    }
+
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+
+                                }
+                            });
+
+                            //переходим в главный экран
+                            Intent intent=new Intent(StartActivity.this,OsnovnoeActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(getContext(),
+                                    "Успешный вход",Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(getContext(),
+                                    "Не удалось войти",Toast.LENGTH_SHORT).show();
+                        }
+                    }});
+    }*/
+
+    //---------------------------------------------------------------------------------
 }
